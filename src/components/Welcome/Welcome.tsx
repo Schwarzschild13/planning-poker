e3acf8c20aa55554e801f33587a0310a6e85a84d
Harikrishnan <harikrishnan.pr@hatio.in>
2021-10-19 14:53:53 +0530

    Add create and join form in welcome

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,11 +1,45 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
+import { useHistory } from "react-router";
+import { v1 as uuidv1 } from "uuid";
+import { AdminContext } from "../../context/Context";
 
 interface WelcomeProps {}
 
 const Welcome: FunctionComponent<WelcomeProps> = () => {
+  const { isAdmin, toggleAdmin } = useContext(AdminContext);
+
+  let gameId: string;
+  const history = useHistory();
+  const createGame = () => {
+    let path = `/${uuidv1()}`;
+    history.push(path);
+    toggleAdmin();
+  };
+
+  const joinGame = (gameId: string) => {
+    let path = `/${gameId}`;
+    history.push(path);
+  };
+
   return (
     <>
       <h1>Welcome</h1>
+      <button onClick={createGame}>Create a game</button>
+      <input
+        type="text"
+        name="joinGame"
+        id="joinGame"
+        onChange={(e) => {
+          gameId = e.target.value;
+        }}
+      />
+      <button
+        onClick={() => {
+          joinGame(gameId);
+        }}
+      >
+        Join
+      </button>
     </>
   );
 };
