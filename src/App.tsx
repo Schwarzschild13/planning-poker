8712c03c888db6fd46e99b702d29cfb2ede682db
Harikrishnan <harikrishnan.pr@hatio.in>
2021-10-19 14:41:51 +0530

    Add routes for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Welcome from "./components/Welcome/Welcome";
+import GameScreenList from "./components/GameScreenList/GameScreenList";
+import GameScreenForm from "./components/GameScreenForm/GameScreenForm";
+import Navbar from "./components/Navbar/Navbar";
+import AdminProvider from "../src/context/Context";
 
 function App() {
-  return <div className="App">planning poker</div>;
+  return (
+    <AdminProvider>
+      <div className="App">
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Welcome />
+            </Route>
+            <Route exact path="/:id">
+              <GameScreenForm />
+            </Route>
+            <Route exact path="/game/:id">
+              <GameScreenList />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </AdminProvider>
+  );
 }
 
 export default App;
